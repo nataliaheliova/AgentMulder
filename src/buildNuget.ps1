2cb127a4317030aaaf88e27cdadbfd7b44a63be9
Marek Linka <marek.linka@outlook.com>
2016-12-13 14:34:54 +0100

    Make solution buildable with new R# SDK (version 106) Add build script for the NuGet package

---
+++ src/buildNuget.ps1
@@ -0,0 +1,22 @@
+# BuildNuget.ps1
+#
+# Builds the AgentMulder nuget package
+#
+
+param ($version = $(throw "Version must be specified"), $waveVersion = $(throw "Wave version must be specified"))
+
+echo "Building AgentMulder version $version for Wave $waveVersion.0";
+
+# resetting working dir to the directory of the script
+# this is necesary as we are using relative paths later on
+$dir = Split-Path $MyInvocation.MyCommand.Path;
+Push-Location $dir;
+[Environment]::CurrentDirectory = $PWD
+
+$nugetPath = ".nuget\NuGet.exe";
+$nugetParams = "pack AgentMulder.nuspec -nopackageanalysis -outputdirectory nuget -properties 'version=$version;waveVersion=$waveVersion'";
+
+echo "Running NuGet: ", "$nugetPath $nugetParams"
+iex "$nugetPath $nugetParams";
+
+echo "AgentMulder $version built successfully";
