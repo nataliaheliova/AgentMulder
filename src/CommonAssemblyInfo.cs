420e73f5a65b93e2c5548c13473e349a8b2d2620
Marek Linka <marek.linka@outlook.com>
2016-12-16 15:32:02 +0100

    Update R# SDK to 2016.3

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright \u00A9 Igal Tabachnik, 2012-2016")]
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("2016.1.0.0")]
+[assembly: AssemblyFileVersion("2016.3.0.0")]
